de54aee830f3ad311a17e1dc8420c7c07b4b07d5
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2018-07-19 22:42:59 +0700

    first

--- entry.js
+++
@@ -1,7 +0,0 @@
-require('./bower_components/angular/angular');
-require('./bower_components/ng-file-upload/ng-file-upload');
-require('./bower_components/ng-file-upload-shim/ng-file-upload-shim');
-
-require('./client/csv-transformer/csv-transformer');
-
-require('./client/csv-transformer/csv-transformer.css');
